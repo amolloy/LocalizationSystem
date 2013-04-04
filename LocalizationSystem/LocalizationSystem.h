e01aad94608b3aee49081d5aed42b32cc8179830
Andy Molloy <asmolloy@me.com>
2013-04-04 16:32:06 -0400

    Initial Commit

---
+++ LocalizationSystem/LocalizationSystem.h
@@ -0,0 +1,13 @@
+//
+//  LocalizationSystem.h
+//  LocalizationSystem
+//
+//  Created by The Molloys on 4/4/13.
+//  Copyright (c) 2013 Andy Molloy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalizationSystem : NSObject
+
+@end
---
+++ LocalizationSystem/LocalizationSystem.m
@@ -0,0 +1,13 @@
+//
+//  LocalizationSystem.m
+//  LocalizationSystem
+//
+//  Created by The Molloys on 4/4/13.
+//  Copyright (c) 2013 Andy Molloy. All rights reserved.
+//
+
+#import "LocalizationSystem.h"
+
+@implementation LocalizationSystem
+
+@end
